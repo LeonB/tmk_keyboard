040de81e842464764ffc290f7f3430857cad82d7
Leon Bogaert <leon@tim-online.nl>
2014-07-13 01:23:04 +0200

    Implemented first layout + custom action_function()

--- keyboard/ergodox/keymap_leonb.h
+++ keyboard/ergodox/keymap_leonb.h
@@ -1,34 +1,41 @@
-/*
- * make -f Makefile.lufa clean
- * make -f Makefile.lufa leonb
- */
-
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * Keymap: Default Layer in QWERTY
      *
      * ,--------------------------------------------------.           ,--------------------------------------------------.
-     * |   ~    |   1  |   2  |   3  |   4  |   5  |   \  |           |   -  |   6  |   7  |   8  |   9  |   0  |   =    |
+     * |  `     |  1   |  2   |  3   |  4   |  5   |      |           |  -   |  6   |  7   |  8   |  9   |  0   |  =     |
      * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
-     * | Tab    |   Q  |   W  |   E  |   R  |   T  | ~L5  |           | ~L6  |   Y  |   U  |   I  |   O  |   P  |   [    |
+     * | Tab    |  Q   |  W   |  E   |  R   |  T   | L1   |           | [    |  Y   |  U   |  I   |  O   |  P   |  \     |
      * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-     * | Tab/Shf|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
-     * |--------+------+------+------+------+------|  L0  |           | ~L7  |------+------+------+------+------+--------|
-     * | LCtrl  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   ]    |
+     * |ctrl/esc|  A   |  S   |  D   |  F   |  G   |------|           |------|  H   |  J   |  K   |  L   |  ;   |  '     |
+     * |--------+------+------+------+------+------|      |           | ]    |------+------+------+------+------+--------|
+     * | Lshift |  Z   |  X   |  C   |  V   |  B   |      |           |      |  N   |  M   |  ,   |  .   |  /   | LShift |
      * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-     *   | ~L5  | ~L2  | Caps | LAlt | LGui |                                       |  Lft |  Up  |  Dn  | Rght | ~L6  |
+     *   | PgUp | PgDn | End  | Home | Alt  |                                       |  Lft |  Dn  |  Up  | Rght |      |
      *   `----------------------------------'                                       `----------------------------------'
      *                                        ,-------------.       ,-------------.
-     *                                        | +L2  | Home |       | PgUp | Del  |
+     *                                        | LGui |      |       |      |      |
      *                                 ,------|------|------|       |------+------+------.
-     *                                 |      |      |  End |       | PgDn |      |      |
-     *                                 | BkSp |  ESC |------|       |------| Enter| Space|
-     *                                 |      |      |  Spc |       | Ins  |      |      |
+     *                                 |      |      |      |       |      |      |      |
+     *                                 | BkSp |  Del |------|       |------| Enter| Space|
+     *                                 |      |      |      |       |      |      |      |
      *                                 `--------------------'       `--------------------'
      *
      *
      *
      ****************************************************************************************************
+     *
+     * HOW TO COMPILE:
+     *
+     * make -f Makefile.lufa clean
+     * make -f Makefile.lufa leonb
+     *
+     ****************************************************************************************************
+     *
+     * Test:
+     * - Alt + F4
+     * - Ctrl + Gui + Up
+     * - Ctrl + Alt + Shift + Lft/Rght
      *
      * For such cases I have Layer1 :)
      * // hint: switch to Layer1 is only at Layer6
@@ -39,93 +46,28 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KEYMAP(  // Layer0: default, leftled:none
         // left hand
-        GRV, 1,   2,   3,   4,   5,   BSLS,
-        FN2, Q,   W,   E,   R,   T,   FN23,
-        FN11,FN28,FN29,FN30,FN31,G,
-        FN12,FN24,FN25,FN26,FN27,B,   HOME,
-        FN21,FN20,CAPS,FN13,FN14,
-                                      FN17,FN19,
+        GRV, 1,   2,   3,   4,   5,   NO,
+        TAB, Q,   W,   E,   R,   T,   NO,
+        FN2, A,   S,   D,   F,   G,
+        LSFT,Z,   X,   C,   V,   B,   NO,
+        PGUP,PGDN,END, HOME,LALT,
+                                      LGUI,NO,
                                            NO,
-                                 FN5, FN6, FN7,
+                                 BSPC,DEL, NO,
         // right hand
              MINS,6,   7,   8,   9,   0,   EQL,
-             FN23,Y,   U,   I,   O,   P,   LBRC,
-                  H,   J,   K,   L,   SCLN,FN15,
-             END, N,   M,   COMM,DOT, SLSH,FN16,
-                       LEFT,UP,  DOWN,RGHT,FN22,
-        PGUP,DEL,
-        PGDN,
-        FN8, FN9, FN10
+             LBRC,Y,   U,   I,   O,   P,   BSLS,
+                  H,   J,   K,   L,   SCLN,QUOT,
+             RBRC,N,   M,   COMM,DOT, SLSH,RSFT,
+                       LEFT,DOWN,UP,  RGHT,FN1,
+        NO,  NO,
+        NO,
+        NO,  ENT, SPC
     ),
 
-    KEYMAP(  // Layer1: Workman layout, leftled:all
+    KEYMAP(  // Layer1: default, leftled:all
         // left hand
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,Q,   D,   R,   W,   B,   TRNS,
-        TRNS,A,   S,   H,   T,   G,
-        TRNS,Z,   X,   M,   C,   V,   TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 TRNS,TRNS,TRNS,
-        // right hand
-             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-             TRNS,J,   F,   U,   P,   4,   TRNS,
-                  Y,   N,   E,   O,   I,   TRNS,
-             TRNS,K,   L,   TRNS,TRNS,TRNS,TRNS,
-                       TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
-    ),
-
-    KEYMAP(  // Layer2: numpad, leftled:mid/blue
-        // left hand
-        TRNS,NO,  NO,  NO,  NO,  PAUS,PSCR,
-        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
-        TRNS,NO,  NO,  NO,  TRNS,NO,
-        TRNS,NO,  NO,  NO,  TRNS,NO,  TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 TRNS,TRNS,TRNS,
-        // right hand
-             SLCK,NLCK,PSLS,PAST,PAST,PMNS,BSPC,
-             TRNS,NO,  P7,  P8,  P9,  PMNS,PGUP,
-                  NO,  P4,  P5,  P6,  PPLS,PGDN,
-             TRNS,NO,  P1,  P2,  P3,  PPLS,PENT,
-                       P0,  PDOT,SLSH,PENT,PENT,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
-    ),
-
-/*
-    // templates to copy from
-
-    KEYMAP(  // LayerN: transparent on edges + hard-defined thumb keys, all others are empty
-        // left hand
-        TRNS,NO,  NO,  NO,  NO,  NO,  NO,
-        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
-        TRNS,NO,  NO,  NO,  NO,  NO,
-        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
-        TRNS,TRNS,TRNS,LALT,LGUI,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 LCTL,LSFT,TRNS,
-        // right hand
-             NO,  NO,  NO,  NO,  NO,  NO,  TRNS,
-             TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
-                  NO,  NO,  NO,  NO,  NO,  TRNS,
-             TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
-                       RGUI,RALT,TRNS,TRNS,TRNS,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,RSFT,RCTL
-    ),
-    KEYMAP(  // LayerN: fully transparent
-        // left hand
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        FN0, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
@@ -143,18 +85,72 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS,
         TRNS,TRNS,TRNS
     ),
-*/
-
 };
 
 /* id for user defined functions & macros */
 enum function_id {
     TEENSY_KEY,
+    CUSTOM_LAYER_TAP_TOGGLE,
 };
 
 /*
  * Fn action definition
  */
 static const uint16_t PROGMEM fn_actions[] = {
-    [0] =   ACTION_FUNCTION(TEENSY_KEY),                    // FN0  - Teensy key
+    [0] =   ACTION_FUNCTION(TEENSY_KEY),                 // FN0  - Teensy key
+    [1] =   ACTION_FUNCTION_TAP(CUSTOM_LAYER_TAP_TOGGLE), // FN1  - Teensy key
+    [2] =   ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),       // FN2  = LCtrl with tap Escape
 };
+
+/*
+ * user defined action function
+ */
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+    // print("action_function called\n");
+    // print("id  = "); phex(id); print("\n");
+    // print("opt = "); phex(opt); print("\n");
+
+    switch (id) {
+        case TEENSY_KEY:
+            clear_keyboard();
+            print("\n\nJump to bootloader... ");
+            _delay_ms(50);
+            bootloader_jump(); // should not return
+            print("not supported.\n");
+            break;
+
+        case CUSTOM_LAYER_TAP_TOGGLE:
+            xprintf("\n\ntap.count: %d", record->tap.count);
+            xprintf("\n\nevent.pressed: %d", record->event.pressed);
+
+            if (record->tap.count > 0) {
+                if (record->event.pressed) {
+                    print("\nlayer_invert");
+                    layer_invert(1);
+                }
+            } else {
+                if (record->event.pressed) {
+                    print("\nlayer_on");
+                    layer_on(1);
+                } else {
+                    print("\nlayer_off");
+                    layer_off(1);
+                }
+            }
+            break;
+    }
+}
+
+/*
+ * ACTION_MODS_TAP_KEY(MOD_RCTL, KC_ENT)
+ * Works as a modifier key while holding, but registers a key on tap(press and
+ * release quickly).
+ */
+
+/*
+ * ACTION_LAYER_TAP_TOGGLE(layer)
+ * Turns on layer momentary while holding and toggles it with serial taps.
+ */
+
+/* ACTION_MODS_ONESHOT(MOD_LSFT) */
